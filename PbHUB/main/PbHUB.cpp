79396706b56413f26698971e93d246a7e1682713
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2023-03-08 16:54:17 +0100

    clean up-

--- PbHUB/main/PbHUB.cpp
+++ PbHUB/main/PbHUB.cpp
@@ -3,7 +3,6 @@
    or drive multiple sets of RGB LEDs.
 */
 
-#include <M5Stack.h>
 #include <Wire.h>
 
 #include "porthub.h"
@@ -16,19 +15,12 @@ uint8_t HUB_ADDR[6] = {HUB1_ADDR, HUB2_ADDR, HUB3_ADDR,
                        HUB4_ADDR, HUB5_ADDR, HUB6_ADDR};
 
 void setup() {
-    M5.begin(true, false, true);
-    M5.Power.begin();
     porthub.begin();
-    M5.Lcd.clear(BLACK);
-    M5.Lcd.setTextColor(WHITE);
-    M5.Lcd.setTextSize(4);
-}
+    }
 
 void loop() {
-    M5.Lcd.clear(BLACK);
     for (int i = 0; i < 6; i++) {
-        M5.Lcd.setCursor(8 * X_OFFSET, (i * 2) * Y_OFFSET);
-        M5.Lcd.printf("%d:%d", i + 1, porthub.hub_a_read_value(HUB_ADDR[i]));
+        porthub.hub_a_read_value(HUB_ADDR[i]);
     }
 
     for (int i = 0; i < 6; i++) {
